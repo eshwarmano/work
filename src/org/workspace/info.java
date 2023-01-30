e48325e3ebdea0ffb14d1914b8223bb41e091134
eshwarmano <eshwarmano1999@gmail.com>
2023-01-30 11:38:24 +0530

    new

--- src/org/workspace/info.java
+++ src/org/workspace/info.java
@@ -3,6 +3,10 @@ package org.workspace;
 public class info {
  public static void main(String[] args) {
 	System.out.println("6669999");
+	System.out.println("6669999777");
+	System.out.println("eshwarsakthi");
+	System.out.println("eshwarsakthi007");
+	System.out.println("eshwarsakthi9");
 }
 
 
